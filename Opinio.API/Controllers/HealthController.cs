fb0a97ee6f2f86f9976b4a2043a5250ceec3b3e1
Mahmoud Basiouny <mahmoud.bassiouny@thinktech-it.com>
2025-09-20 01:06:18 +0300

    init

---
+++ Opinio.API/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Opinio.API.Controllers;
+
+[ApiController]
+public class HealthController : ControllerBase
+{
+    [HttpGet("api/ping")]
+    public IActionResult Ping()
+    {
+        return Ok(new { message = "Opinio API is running 🚀" });
+    }
+}
---
+++ Opinio.API/ServiceConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace Opinio.API;
+
+public static class ServiceConfiguration
+{
+    public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        //var dbConnectionString = config.GetValue<string>("TradingDBConnection") ?? throw new Exception("DBConnection configuration not found");
+        //services.AddDbContext<TradingDbContext>(options => options.UseNpgsql(dbConnectionString).LogTo(log => Debug.WriteLine(log))
+        //        .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking))
+        //        .AddScoped<BaseDbContext>(sp => sp.GetRequiredService<TradingDbContext>())
+
+
+        //services.AddHttpContextAccessor();
+
+        //Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
+        //services.AddTransient<Microsoft.Extensions.Logging.ILogger>(_ => _.GetRequiredService<ILogger<Program>>());
+
+        return services;
+    }
+}
